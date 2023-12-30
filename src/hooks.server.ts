9208bd13de0b93ac2e31b919d433ed6a4035d529
Dasha <davaletova@gmail.com>
2023-12-30 14:41:29 +0400

    finished description + fixed bug

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,17 +2,17 @@ import type { Handle } from "@sveltejs/kit";
 import { adminDB } from "$lib/server/admin";
 
 export const handle = (async ({ event, resolve }) => {
-    let sessionCookie = event.cookies.get("userID");
+    let sessionCookie = event.cookies.get("__session");
 
-    if (!sessionCookie) {
+    if (sessionCookie == null) {
+        console.log(sessionCookie)
         const user = await adminDB.collection("users").add({ducks: []});
         const expiresIn = 60 * 60 * 24 * 30 * 1000;
         const options = { maxAge: expiresIn, httpOnly: true, secure: false, path: '/' };
-        event.cookies.set("userID", user.id, options);
+        event.cookies.set("__session", user.id, options);
         sessionCookie = user.id;
     }
     
-    // const userDoc = await adminDB.collection("users").doc(sessionCookie!).get();
     event.locals.userID = sessionCookie;
     return resolve(event);
 
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -37,24 +37,3 @@ export const actions = {
     },
 } satisfies Actions;
 
-
-// export const load = (async ({ locals, params }) => {
-    
-//     const uid = locals.userID; 
-
-//     if(!uid) {
-//         throw redirect(301, "/login")
-//     }
-
-//     const userDoc = await adminDB.collection("users").doc(uid!).get();
-//     const { username, bio } = userDoc.data()!;
-
-//     if(params.username !== username) {
-//         throw error(401, "That username does not belong to you");
-//     }
-
-//     return {
-//         bio,
-//     };
-
-// }) satisfies PageServerLoad;
